752b029976852f1f5fde75da3230cd80f846bc2f
khm <khm6104kr@naver.com>
2016-07-26 11:48:09 +0900

    버그 수정 - 중복된 번호 문제

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 function generate() {
 	var numbers = [];
 	for(var i=0; i<6; i++) {
-		numbers.push(Math.floor(Math.random() * 45) + 1);
+		var number = numbers.push(Math.floor(Math.random() * 45) + 1);
+		if(contains(numbers, number)) {
+			i--;
+		} else {
+			numbers.push(number);
+		}
+
 	}
 	return numbers;
 }
+
+function contains(numbers, number) {
+	for(var i=0; i<numbers.length; i++) {
+		if(numbers[i] === number) {
+			return true;
+		}
+	}
+	return false;
+}
+
